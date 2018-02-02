cf2e14cc874d6d62ccdfb8906cbaa9d844ae0015
Hanope <heemanghan@gmail.com>
2018-02-02 23:55:05 +0900

    @Builder: 클래스를 Builder 패턴으로 만들어주는 어노테이션
    Controller에서 Bean을 주입 받을 때 @Autowired보다는 생성자를 이용해서 주입하는게 좋음!
    
    Request, Response를 Entity를 직접 이용하는 것보다는 Dto를 따로 정의하여 사용하는 것이 좋음. (N:M 관계경우 복잡하기 때문에 Dto를 이용하는 방법이 좋음)
    
    JPA Auditing을 이용하면 데이터 생성, 수정시간을 자동으로 관리해줌.
    @MappedSuperclass: JPA Entity 클래스들이 BaseTimeEntity를 상속할 경우 필드들(createdDate, modifiedDate)도 컬럼으로 인식하도록 함.
    @EntityListeners(AuditingEntityManager.class): Auditing 기능을 포함함.
    Hibernate 버전 문제로 자바8의 LocalDateTime이 DB에 제대로 저장되지 않는 문제가 발생하므로 Hibernate 5.2.10 버전 이상을 이용해야 함!

--- src/main/java/com/hope/webservice/web/WebRestController.java
+++ src/main/java/com/hope/webservice/web/WebRestController.java
@@ -1,13 +1,27 @@
 package com.hope.webservice.web;
 
+import com.hope.webservice.domain.posts.PostsRepository;
+import com.hope.webservice.domain.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+    }
+
 }
--- src/test/java/com/hope/webservice/ApplicationTest.java
+++ src/test/java/com/hope/webservice/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.hope.springwebservice;
+package com.hope.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringWebserviceApplicationTests {
+public class ApplicationTest {
 
 	@Test
 	public void contextLoads() {
